c2de312c1ac1b739dbfa691b15a3662a26795745
Michael John Reboya <michaeljohnreboya@gmail.com>
2021-04-13 08:51:36 +0800

    Update about-template.php

--- wp-content/themes/blank/template/about-template.php
+++ wp-content/themes/blank/template/about-template.php
@@ -9,6 +9,8 @@
 </div>
 
 <div class="container classnisir">
-	
+	<h1>Its all about shits</h1>
+	<h3>dont panic, its oraganic</h3>
+	<p>shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit shit </p>
 </div>
  <?php get_footer(); ?>
